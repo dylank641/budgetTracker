55d74a371fa35414729c6626590aac1ce26fd951
James Killelea <dylan.killelea@yahoo.com>
2022-05-18 18:48:07 -0500

    added manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -28,3 +28,42 @@ function saveRecord(record) {
     //adds record
     budgetObjectStore.add(record);
 }
+
+function uploadBudget() {
+    //opens transaction
+    const transaction = db.transaction(['big_funds'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('big_funds');
+
+    //gets all records and gives them a variable
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['big_funds'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('big_funds');
+                    // clears items
+                    budgetObjectStore.clear();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+window.addEventListener('online', uploadBudget);
