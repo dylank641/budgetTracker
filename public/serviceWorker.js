9f263a5b2f295fab9b0ad2535dab6c820bb2b552
James Killelea <dylan.killelea@yahoo.com>
2022-05-19 11:24:12 -0500

    finished service file

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -18,18 +18,46 @@ const FILES_TO_CACHE = [
     // ".icons/icon-512x512.png"
 ];
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
+self.addEventListener('fetch', function (event) {
+    console.log('Feteching Request: ' + event.request.url)
+    event.respondWith(
+        caches.match(event.request).then(function (request) {
             if (request) {
-                console.log('responding with cache : ' + e.request.url)
+                console.log('Cache: ' + event.request.url)
                 return request
             } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
+                console.log('File isn"t cached--finding: ' + event.request.url)
+                return fetch(event.request)
             }
 
         })
     )
 })
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+// delete outdated 
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('Deleting cache: ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
+});
